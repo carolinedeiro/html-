3574804f429d833e0afaf2e4319d912e8e893f73
Caroline Sacramento Deiró <carolinesacramento@ifba.edu.br>
2017-11-07 19:49:53 -0200

    Inicializando repositorio

---
+++ botão do mercado.js
@@ -0,0 +1,27 @@
+const funcao = function (){
+            let quantidade = document.getElementById (" insira a quantidade");
+            if( quantidade.value.length == 0)
+            alert("quantidade, " + acc);
+            else
+                  
+                    alert (" Valor total, " + quantidade.Value);
+            
+            
+
+        };
+
+        let botao = document.querySelector ('# botao');
+        botao.addEventListener ( "clik", (nome, funcao);
+
+             var acc = 0;
+
+            var arrayquantidade = Document.querySelectorall (".quantidade");
+
+            for ( var i = 0; i< arreyquantidade.length; i++){
+                    arrayquantidade[i].Value;
+                    acc = acc + arrayquantidade
+
+            }
+          
+                
+            }; 
---
+++ mercado.js
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title> Testa </title>
+    </head>
+
+    <body>
+        <label id = "label"> NOME: </label>
+        <input id = "nome" nome = "NOME" placeholder = "insira seu nome"> </input>
+        <button id = "botao"> Enviar </button>
+    
+    <script src = "script.is" type = "text/javascript" ></script>
+
+    </body>
+</html>
